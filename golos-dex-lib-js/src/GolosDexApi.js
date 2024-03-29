8dda36b32cef56a9256e0938770f749958b2ee06
1aerostorm <aerostorm1@gmail.com>
2024-03-29 06:10:47 +0300

    JS - Add getExchange utils

--- golos-dex-lib-js/src/GolosDexApi.js
+++ golos-dex-lib-js/src/GolosDexApi.js
@@ -31,7 +31,7 @@ class GolosDexApi {
             this.golos = typeof('global') !== undefined && global.golos
         }
         if (!this.golos) {
-            console.warn('GolosDexApi initialized without golos library. apidexExchange will not work.')
+            console.warn('GolosDexApi initialized without golos library. apidexExchange/getExchange will not work.')
         }
         this.opts = opts
         if (this.golos && opts.patch_golos) {
@@ -159,6 +159,84 @@ class GolosDexApi {
             console.error('apidexExchange', err)
             return null
         }
+    }
+
+    getExchange = async (query) => {
+        const { node, ...rest } = query
+
+        if (!this.golos) {
+            throw new Error('getExchange not supported - GolosDexApi initialized without golos-lib-js')
+        }
+
+        let eapi = this.golos.api
+        if (!eapi || !eapi.getExchange) {
+            throw new Error('golos-lib-js is too old. To support getExchange it should be at least >=0.9.66')
+        }
+        if (node) {
+            eapi = new eapi.Golos()
+            eapi.setWebSocket(node)
+        }
+
+        return await eapi.getExchange(rest)
+    }
+
+    ORDER_MAX_EXPIRATION = 0xffffffff
+
+    makeOrderID = () => {
+        return Math.floor(Date.now() / 1000)
+    }
+
+    makeExchangeTx = async (exchangeSteps, opts) => {
+        if (!this.golos) {
+            throw new Error('makeExchangeTx not supported - GolosDexApi initialized without golos-lib-js')
+        }
+        const { Asset, Price } = this.golos.utils
+        if (!Asset || !Price) {
+            throw new Error('golos-lib-js is too old. Recommended is >=0.9.31')
+        }
+
+        const defOpts = {
+            op_type: 'limit_order_create',
+            orderid: (op, i, ops, step) => {
+                return this.makeOrderID()
+            }
+        }
+        opts = {...defOpts, ...opts}
+
+        const ops = []
+        let i = 0
+        for (const step of exchangeSteps) {
+            const op = {}
+
+            const copyField = (key, defVal) => {
+                if (key in opts) {
+                    if (opts[key] !== undefined) {
+                        op[key] = opts[key]
+                    }
+                } else if (defVal !== undefined) {
+                    op[key] = defVal
+                }
+            }
+
+            copyField('owner')
+            if ('orderid' in opts) op.orderid = opts.orderid
+
+            op.amount_to_sell = step.sell
+
+            const prc = await new Price(step.limit_price)
+            op.min_to_receive = Asset(step.sell).mul(prc).toString()
+
+            copyField('fill_or_kill', false)
+
+            op.expiration = opts.expiration || this.ORDER_MAX_EXPIRATION
+
+            if (isFunction(opts.orderid)) {
+                op.orderid = await opts.orderid(op, i++, ops, step)
+            }
+
+            ops.push([opts.op_type, op])
+        }
+        return ops
     }
 }
 
